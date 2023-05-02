222f970573c6623fb752f156e2e4f39104d39da4
Mohammad-Shiyyab <mohammadshiyab1996@gmail.com>
2023-05-02 23:11:33 +0300

    solve

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
+# mohammad shiyab
 # snakes-cafe
+
+LAB - Class 01
+
+Project: snakes-cafe
+
+Author:  mohammad shiyab
+
+Links and Resources
+
+back-end server url (when applicable)
+
+front-end application (when applicable)
+
+Setup
+
+.env requirements (where applicable)
+i.e.
+
+PORT - Port Number
+
+DATABASE_URL - URL to the running Postgres instance/db
+How to initialize/run your application (where applicable)
+
+python snackes_cafe.py
+
+How to use your library (where applicable)
+
+Tests
+
+How do you run tests?
+
+python snackes_cafe.py
+
+Any tests of note?
+
+ 
--- snackes_cafe.py
+++ snackes_cafe.py
@@ -1,1 +1,79 @@
-print("mohamed")
+# **************************************
+# **    Welcome to the Snakes Cafe!   **
+# **    Please see our menu below.    **
+# **
+# ** To quit at any time, type "quit" **
+# **************************************
+
+def intro():
+    print('''
+**************************************
+**    Welcome to the Snakes Cafe!   **
+**    Please see our menu below.    **
+**
+** To quit at any time, type "quit" **
+**************************************
+
+Appetizers
+----------
+Wings
+Cookies
+Spring Rolls
+
+Entrees
+-------
+Salmon
+Steak
+Meat Tornado
+A Literal Garden
+
+Desserts
+--------
+Ice Cream
+Cake
+Pie
+
+Drinks
+------
+Coffee
+Tea
+Unicorn Tears
+
+***********************************
+** What would you like to order? **
+***********************************
+''')
+
+Appetizers=["wings","cookies","spring rolls"]
+Entrees=["salmon","steak","meat Tornado","a literal garden"]
+Desserts=["ice cream","cake","pie"]
+Drinks=["coffee","tea","unicorn tears"]
+def user_insertion():
+    user_input=input(">")
+    return user_input
+
+def main():
+    intro()
+    user_input = ""
+    w=[]
+    while(user_input.lower() != "quit"):
+        x = 0
+        user_input = user_insertion()
+        w.append(user_input.lower())
+        for a in w:
+            if user_input.lower() == a:
+                x = x + 1
+        if user_input.lower() in Appetizers or user_input.lower() in Entrees or user_input.lower() in Desserts or user_input.lower() in Drinks:
+            print('*',x , "order of", user_input,"has been add to your meal *")
+        else:       
+            print("sorry we dint hane this items !")
+            user_input = user_insertion()
+
+    end_application()
+
+def end_application():
+    print("thanks for using snackes cafe applecation !")
+
+if __name__=="__main__":
+    
+    main()
